449f234296a562add34588469cb4e2506cfc8378
sari.ellila <ellila89@hotmail.com>
2023-12-05 22:35:58 +0100

    mission_page

--- client/src/views/Mission.js
+++ client/src/views/Mission.js
@@ -15,16 +15,27 @@ const Mission = () => {
 </div>
 
 <div class="mission_amazon_charities">
-<img src="./images/Amazon_frontlines_jpg"/>
+    <img src="./images/Amazon_frontlines.jpg"/>
+    <div class="amazon_charity_text">
+    <h1 class="amazon_mission_title">AMAZON</h1>
     <h1>Amazon Frontlines</h1>
+    <h2>Amazon Frontlines is a charity helping the indigenous people in Ecuador, Peru and Colombia to overcome fights with the governments, taking back owership of land and protecting the Amazon.</h2>
+    <button class="amazon_webpage_link">Visit the Webpage</button>
+    </div>
+</div>
+
+<div class="mission_wildlife_charities">
+    <div class="wildlife_charity_text">
+    <h1 class="wildlife_mission_title">WILDLIFE</h1>
+    <h1>Only One</h1>
+    <h2>Only One is an organization that plants mangrove trees with peoples' donations. Mangrove trees  are super trees that reduce 4-10x more carbon than rainforests, and restoring the forests ptect thousands of species.</h2>
+    <button class="wildlife_webpage_link">Visit the Webpage</button>
+    </div>
+    <img src="./images/mangrove.jpg"/>
 </div>
 
 <div class="mission_ocean_charities">
     <h1>OCEAN CHARITIES</h1>
-</div>
-
-<div class="mission_wildlife_charities">
-    <h1>WILDLIFE CHARITIES</h1>
 </div>
 
         </div>
