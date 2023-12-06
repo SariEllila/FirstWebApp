c54cc6cdfd13d7a61e9c1cd206f4c8bf3950f349
sari.ellila <ellila89@hotmail.com>
2023-12-06 11:08:57 +0100

    _

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,14 +8,20 @@ const Header = () => {
 <nav class="navbar">
 
     <div class="homelogo">
-    <Link to="/"><h1>GIFTS for EARTH</h1></Link>
+    <Link to="/"><h1 class="homelogo">GIFTS for EARTH</h1></Link>
     </div>
 
     <div class="nav_items">
+
     <Link to="/mission"><button class="nav_item_mission">Mission</button></Link>
     <Link to="/login"><button class="nav_item_account">User Account</button></Link>
     <Link to="/store"><button class="nav_item_store">Online Store</button></Link>
+
+    <div class="cart_counter">
     <Link to="/cart"><img class="cart_icon" src="./logos/cart.png"/></Link>
+    <h2>0</h2>
+    </div>
+
     </div>
 
 </nav>
--- client/src/views/Mission.js
+++ client/src/views/Mission.js
@@ -7,7 +7,7 @@ const Mission = () => {
 
 <div class="mission_intro">
         <div class="mission_title">
-            <h1>Mission</h1> 
+            <h1 class=""> OUR MISSION</h1> 
         </div>
         <div class="mission_intro_text">
             <h2>Our mission is to give to small charities around the world doing important work for our environment. We target charities from three areas: Amazon, ocean, and wildlife preservation. See our different charity targets below and visit the links to learn more on their websites.</h2>
@@ -15,11 +15,11 @@ const Mission = () => {
 </div>
 
 <div class="mission_amazon_charities">
-    <img src="./images/Amazon_frontlines.jpg"/>
+    <img src="./images/Amazon_frontlines.jpg" alt="indigenous people marching in bright colored facepaints and outfits demonstrating for their causes in the street"/>
     <div class="amazon_charity_text">
     <h1 class="amazon_mission_title">AMAZON</h1>
     <h1>Amazon Frontlines</h1>
-    <h2>Amazon Frontlines is a charity helping the indigenous people in Ecuador, Peru and Colombia to overcome fights with the governments, taking back owership of land and protecting the Amazon.</h2>
+    <h2>Amazon Frontlines is a charity helping the indigenous people in Ecuador, Peru and Colombia to overcome fights with the governments, taking back ownership of land and protecting the Amazon.</h2>
     <button class="amazon_webpage_link">Visit the Webpage</button>
     </div>
 </div>
@@ -28,19 +28,24 @@ const Mission = () => {
     <div class="wildlife_charity_text">
     <h1 class="wildlife_mission_title">WILDLIFE</h1>
     <h1>Only One</h1>
-    <h2>Only One is an organization that plants mangrove trees with peoples' donations. Mangrove trees  are super trees that reduce 4-10x more carbon than rainforests, and restoring the forests ptect thousands of species.</h2>
+    <h2>Only One is an organization that plants mangrove trees with peoples' donations. Mangrove trees  are super trees that reduce 4-10x more carbon than rainforests, and restoring the forests protect thousands of species.</h2>
     <button class="wildlife_webpage_link">Visit the Webpage</button>
     </div>
-    <img src="./images/mangrove.jpg"/>
+    <img src="./images/mangrove.jpg" alt="painting of river with mangrove trees on the sides and different animals in the water and on the trees"/>
 </div>
 
 <div class="mission_ocean_charities">
-    <h1>OCEAN CHARITIES</h1>
+<img src="./images/Ocean_cleanup.jpg" alt="a beach full of plastic waste and some people walking between it"/>
+    <div class="ocean_charity_text">
+    <h1 class="ocean_mission_title">OCEAN</h1>
+    <h1>Ocean Sole</h1>
+    <h2>An organization cleaning up the beaches of Kenya by making products from flipflops left on beaches. The aim is to recycle over a million fliflops a year, over one tonne of styrofoam per month.</h2>
+    <button class="ocean_webpage_link">Visit the Webpage</button>
+    </div>
 </div>
 
         </div>
     )
-
 }
 
 export default Mission;
