fa0ae7e086602c9a7f7d9cd8554d94f4fc1d7684
sari.ellila <ellila89@hotmail.com>
2023-12-11 17:39:12 +0100

    deployment

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,6 @@
-export const URL = 'http://localhost:4040'
+// export const URL = 'http://localhost:4040'
+
+export const URL =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+    ? "http://localhost:3030"
+    : "https://frightened-pear-shorts.cyclic.app";
