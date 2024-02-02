1ff10f0fef7725108269ae0f8d93983dc938a0e0
sari.ellila <ellila89@hotmail.com>
2024-02-03 00:07:29 +0700

    020224-localhost-changed-to-regex

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 // export const URL = 'http://localhost:4040'
 
-export const URL =
+    export const URL =
   !process.env.NODE_ENV || process.env.NODE_ENV === "development"
-    ? "http://localhost:4040"
+    ? `http://localhost:${process.env.PORT || 4040}`
     : "https://frightened-pear-shorts.cyclic.app";
